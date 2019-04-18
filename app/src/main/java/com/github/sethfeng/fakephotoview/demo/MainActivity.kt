13c6aaf7ffcff8a2d04c657bc094183ca016369b
fengshenzhu <fengshenzhu@gmail.com>
2019-04-18 11:19:26 +0800

    Add demo

--- app/src/main/java/com/github/sethfeng/fakephotoview/demo/MainActivity.kt
+++ app/src/main/java/com/github/sethfeng/fakephotoview/demo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.github.sethfeng.fakephotoview.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView.ScaleType
+import com.example.atemktx.photoview.FakeDrawable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fakePhotoView.apply {
+            setScaleType(ScaleType.CENTER)
+            setImageDrawable(FakeDrawable())
+        }
     }
 }
